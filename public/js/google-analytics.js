0cc8772680b75082f37f063d0e047fc279ff6617
Tilen Komel <tilen.komel10@gmail.com>
2021-04-02 13:04:28 +0200

    Google analyitics removed

--- public/js/google-analytics.js
+++
@@ -1,9 +0,0 @@
-import "https://www.googletagmanager.com/gtag/js?id=G-1LC7ZQ6R27";
-
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-	dataLayer.push(arguments);
-}
-gtag('js', new Date());
-
-gtag('config', 'G-1LC7ZQ6R27');

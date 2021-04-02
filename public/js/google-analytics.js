851f4f2e25c6a1df94f48e79cb6ad76f2200340a
Tilen Komel <tilen.komel10@gmail.com>
2021-04-02 12:31:12 +0200

    CSP

---
+++ public/js/google-analytics.js
@@ -0,0 +1,9 @@
+import "https://www.googletagmanager.com/gtag/js?id=G-1LC7ZQ6R27";
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+	dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-1LC7ZQ6R27');

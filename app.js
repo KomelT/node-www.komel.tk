61462ff9356182a03b4eb983965f14272b2b5434
Tilen Komel <tilen.komel10@gmail.com>
2021-04-02 08:34:25 +0200

    CSP Updated

--- app.js
+++ app.js
@@ -24,74 +24,13 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
-app.use(
-    contentSecurityPolicy({
-        "directives": {
-            "frame-ancestors": [
-                "'none'"
-            ],
-            "block-all-mixed-content": [],
-            "default-src": [
-                "'none'"
-            ],
-            "script-src": [
-                "'self'",
-                "'report-sample'",
-                "'unsafe-inline'",
-                "https://*.googletagmanager.com/",
-                "https://*.gstatic.com/",
-                "https://*.google.com/"
-            ],
-            "style-src": [
-                "'self'",
-                "'report-sample'",
-                "'unsafe-inline'",
-                "https://*.googleapis.com/"
-            ],
-            "object-src": [
-                "'none'"
-            ],
-            "frame-src": [
-                "https://*.google.com/"
-            ],
-            "child-src": [
-                "'none'"
-            ],
-            "img-src": [
-                "'self'"
-            ],
-            "font-src": [
-                "'self'",
-                "https://*.gstatic.com/"
-            ],
-            "connect-src": [
-                "'none'"
-            ],
-            "manifest-src": [
-                "'none'"
-            ],
-            "base-uri": [
-                "'self'"
-            ],
-            "form-action": [
-                "'none'"
-            ],
-            "media-src": [
-                "'none'"
-            ],
-            "prefetch-src": [
-                "'none'"
-            ],
-            "worker-src": [
-                "'none'"
-            ],
-            "report-uri": [
-                "https://gate.rapidsec.net/g/r/csp/259afaa7-9e8a-4a1f-8cfe-93602f437c5d/0/2/3?sct=9c701142-6231-49aa-b6dd-0dfe7a9f0fee&dpos=report"
-            ]
-        },
-        "reportOnly": true
-    })
-);
+app.use(function (req, res, next) {
+    res.setHeader(
+        "content-security-policy-report-only",
+        "frame-ancestors 'none'; block-all-mixed-content; default-src 'none'; script-src 'self' 'report-sample' 'unsafe-inline' https://*.googletagmanager.com/ https://*.gstatic.com/ https://*.google.com/; style-src 'self' 'report-sample' 'unsafe-inline' https://*.googleapis.com/; object-src 'none'; frame-src https://*.google.com/; child-src 'none'; img-src 'self'; font-src 'self' https://*.gstatic.com/; connect-src 'none'; manifest-src 'none'; base-uri 'self'; form-action 'none'; media-src 'none'; prefetch-src 'none'; worker-src 'none'; report-uri https://gate.rapidsec.net/g/r/csp/259afaa7-9e8a-4a1f-8cfe-93602f437c5d/0/2/3?sct=9c701142-6231-49aa-b6dd-0dfe7a9f0fee&dpos=report"
+    ),
+        next();
+});
 
 // Allow direct queries to folder "public"
 app.use(express.static("public"))

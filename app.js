0e09425ee35e0de1152f7690fc38d9d0f1d513ec
Tilen Komel <tilen.komel10@gmail.com>
2021-04-03 09:29:09 +0200

    Discord Analyitics

--- app.js
+++ app.js
@@ -101,26 +101,30 @@ app.use(express.static("public"))
 
 // Routes to serve diferent sites
 app.get('/', (req, res) => {
+    webhookClient.send("[www.komel.tk] Someone is looking /");
     res.set('Content-Security-Policy', 'script-src *');
     res.sendFile(path.join(__dirname + '/html/index.html'));
 });
 
 app.get('/about', (req, res) => {
+    webhookClient.send("[www.komel.tk] Someone is looking /about");
     res.sendFile(path.join(__dirname + '/html/about.html'));
 });
 
 app.get('/portfolio', (req, res) => {
+    webhookClient.send("[www.komel.tk] Someone is looking /portfolio");
     res.sendFile(path.join(__dirname + '/html/portfolio.html'));
 });
 
 app.get('/contact', (req, res) => {
+    webhookClient.send("[www.komel.tk] Someone is looking /contacts");
     res.set('Content-Security-Policy', 'script-src https://*');
     res.sendFile(path.join(__dirname + '/html/contact.html'));
 });
 
 // POST request to send Discord message
 app.post("/send", (req, res) => {
-
+    webhookClient.send("[www.komel.tk] Someone sent POST request to /send");
     // Test if origin is right
     if (allowedOrigins.indexOf(req.header("Origin")) === -1) {
         res.send("Origin '" + req.header("Origin") + "' not allowed!")
@@ -190,14 +194,17 @@ app.post("/send", (req, res) => {
 })
 
 app.get("*", (req, res) => {
+    webhookClient.send("[www.komel.tk] Someone got 404 error");
     res.sendFile(path.join(__dirname + '/html/error/404.html'));
 })
 
 app.post("*", (req, res) => {
+    webhookClient.send("[www.komel.tk] Someone sent POST request to /*");
     res.status(405).send("Method not allowed");
 })
 
 app.put("*", (req, res) => {
+    webhookClient.send("[www.komel.tk] Someone sent PUT request to /*");
     res.status(405).send("Method not allowed");
 })
 

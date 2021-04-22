13d7f0bdfe70834deb9d1294d55ac189aa76052e
Tilen Komel <tilen.komel10@gmail.com>
2021-04-22 10:06:10 +0200

    Google Analytics

--- public/js/google-analytics.js
+++ public/js/google-analytics.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
-gtag('config', 'G-XJGWP9H1C2');
+gtag('config', 'G-S793JBVKTJ');
